557359cacd8534347e262db1b4725b5b2f73b1eb
Luke Harmon <lukeh@uidaho.edu>
2014-05-26 17:27:59 -0300

    documented mustelid data

---
+++ R/mustelid.R
@@ -0,0 +1,15 @@
+#' Dataset on mustelids and their fossil diversities.
+#' 
+#' A dataset with a phylogenetic tree and fossil diversity data for mustelids.
+#' 
+#' \itemize{
+#'   \item mustelid.tree. Phylogenetic tree of whales, 46 tips  
+#'   \item mustelid.richness. Includes both fossils and extant taxa
+#'   \item mustelid.ext.richness. Richness of extant clades
+#' }
+#' 
+#' @docType data
+#' @keywords datasets
+#' @format One phylo objects and two dataframes
+#' @name mustelid
+NULL
